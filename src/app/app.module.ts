f4064cfa76a553c650f971cc563252840dea567f
RaulGabG <raulgguanca@gmail.com>
2022-04-25 21:10:18 -0300

    Nuevo portfolio realizado con Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AcercadeComponent } from './componentes/acercade/acercade.component';
+import { EduExpComponent } from './componentes/edu-exp/edu-exp.component';
+import { ProgressbarComponent } from './componentes/progressbar/progressbar.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    AcercadeComponent,
+    EduExpComponent,
+    ProgressbarComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
